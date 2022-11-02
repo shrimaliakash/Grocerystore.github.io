79e8b6bff54c48f3bb684d50922bba5ef67a3bab
Akash Govindbhai Shrimali <akash@nuwaveindia.net>
2022-11-03 00:41:48 +0530

    products section complete

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ document.querySelector("#login-btn").onclick = () =>{
 
 let navbar = document.querySelector('.navbar');
 
-navbar.querySelector("#menu-btn").onclick = () =>{
+document.querySelector("#menu-btn").onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
@@ -40,3 +40,24 @@ window.onscroll = () =>{
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});

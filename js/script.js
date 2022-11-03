e3263211eb87a69597ac71593d95ddb8195b3892
Akash Govindbhai Shrimali <akash@nuwaveindia.net>
2022-11-04 01:17:37 +0530

    review section complet

--- js/script.js
+++ js/script.js
@@ -61,3 +61,24 @@ var swiper = new Swiper(".product-slider", {
       },
     },
 });
+
+var swiper = new Swiper(".review-slider", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
